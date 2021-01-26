7266703ac5e9406a7c43c482780d1403874dede7
JC <bonassin@gmail.com>
2021-01-26 21:17:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,7 @@ class Scraper
 
   end
 
-  def self.scrape_profile_page(profile_url)
+  def self.scrape_profile_page(profile_slug)
     student = {}
     profile_page = Nokogiri::HTML(open(profile_slug))
     links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
